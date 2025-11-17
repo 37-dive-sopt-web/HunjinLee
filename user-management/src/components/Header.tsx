62f9878597552b8d29cf5a143b591a0c3413a8d6
huniversal <hunivers0523@naver.com>
2025-11-18 02:39:50 +0900

    feat: 헤더 컴포넌트 기초 세팅

--- user-management/src/components/Header.tsx
+++ user-management/src/components/Header.tsx
@@ -1,11 +1,43 @@
-import React from 'react'
+import { NavLink, useNavigate } from "react-router-dom";
 
-const Header = () => {
+interface HeaderProps {
+  userName: string;
+}
+
+const Header = ({ userName }: HeaderProps) => {
+  const navigate = useNavigate();
+
+  const handleMyInfo = () => {
+    navigate('/mypage');
+  }
+
+  const handleMembers = () => {
+    navigate('/mypage/members');
+  }
+
+  const handleLogout = () => {
+    console.log('로그아웃');
+    navigate('/login');
+  }
+
+  const handleDeleteAccount = () => {
+    // TODO : 회원탈퇴 모달 열기
+    console.log('회원탈퇴를 클릭하였습니다.');
+  }
+
   return (
-    <div>
-      
-    </div>
-  )
+    <header>
+      <div>
+        <span>안녕하세요, {userName}</span>
+      </div>
+      <nav>
+        <NavLink to="/mypage">내 정보</NavLink>
+        <NavLink to="/mypage/members">회원 조회</NavLink>
+        <button onClick={handleLogout}>로그아웃</button>
+        <button onClick={handleDeleteAccount}>회원탈퇴</button>
+      </nav>
+    </header>
+  );
 }
 
 export default Header
