b14ccc3d855f2978e64be6636961c6e3e8a209c8
huniversal <hunivers0523@naver.com>
2025-11-19 00:33:47 +0900

    feat: 회원조회 라우팅 세팅

--- user-management/src/router/index.tsx
+++ user-management/src/router/index.tsx
@@ -2,7 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import LoginPage from '../pages/LoginPage';
 import SignupPage from '../pages/SignupPage';
 import MyPage from '../pages/MyPage';
-// import MembersPage from '../pages/MembersPage';
+import MembersPage from '../pages/MembersPage';
 
 export const router = createBrowserRouter([
   {
@@ -21,9 +21,8 @@ export const router = createBrowserRouter([
     path: "/mypage",
     element: <MyPage />,
   },
-  // TODO : 세팅 후 추가
-  // {
-  //   path: "/mypage/members",
-  //   element: <MembersPage />,
-  // },
+  {
+    path: "/mypage/members",
+    element: <MembersPage />,
+  },
 ]);
